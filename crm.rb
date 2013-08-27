84056135495f03b864cc5197c560731580b42001
lrzuniga <lrzuniga@gmail.com>
2013-08-27 09:35:49 -0400

    added methods to display all attributes

--- crm.rb
+++ crm.rb
@@ -2,7 +2,7 @@ require_relative "database"
 require_relative "contact"
 
 class CRM
-	
+
 	def initialize(crm_name)
 		@crm_name = crm_name
 		@db = Database.new()
@@ -19,9 +19,8 @@ class CRM
 		puts "[2] Modify an existing contact"
 		puts "[3] Delect a contact"
 		puts "[4] Display all the contacts"
-    	puts "[5] Display an attribute" 
+    	puts "*[5] Display an attribute" 
     	puts "[6] Exit"
-    	puts "[7] Find by ID"
     	puts
     	puts "Select an option:"
 	end
@@ -65,11 +64,48 @@ class CRM
 	end
 
 	def modify_existing_contact
-		puts "Enter ID of contact to be modified"
-		puts @db.all_db_contacts
-		contact_to_be_modified = gets.chomp.to_i
-		@db.modify_existing_contact(contact_to_be_modified)
-	end
+		puts "Enter ID of contact to be modified:" 
+    contact_to_be_modified = gets.chomp.to_i
+    
+      puts "[1] first name" 
+      puts "[2] last name" 
+      puts "[3] email"
+      puts "[4] note" 
+      puts "[5] id" 
+      puts "Select attribute to modify"
+      attribute_to_be_modified = gets.chomp
+
+    case attribute_to_be_modified
+      when "1" then 
+                    puts "input new first_name"
+                    first_name = gets.chomp
+                    @db.modify_first_name(first_name, contact_to_be_modified) 
+                    main_menu
+      when "2" then
+                    puts "input new last_name"
+                    last_name = gets.chomp
+                    @db.modify_last_name(last_name, contact_to_be_modified) 
+                    main_menu
+      when "3" then
+                    puts "input new email"
+                    email = gets.chomp
+                    @db.modify_email(email, contact_to_be_modified) 
+                    main_menu
+      when "4" then
+                   puts "input new note"
+                    note = gets.chomp
+                    @db.modify_note(note,contact_to_be_modified) 
+                    main_menu 
+
+      when "5" then
+                    puts "input new id"
+                    new_id = gets.chomp
+                    @db.modify_id(new_id, contact_to_be_modified) 
+                    main_menu
+                  else
+                  	main_menu
+                  end
+    end
 
 	def delete_contact
 		puts "Select contact do delete by ID number"
@@ -79,9 +115,35 @@ class CRM
 		main_menu
 	end
 
-	def display_attribute
-		raise "Not yet implementd"
-	end
+def display_attribute
+    puts "[1] Name" 
+    puts "[2] email" 
+    puts "[3] note"
+    puts "[4] ID" 
+    puts "Select attribute to display:"
+    attribute_to_be_displayed = gets.chomp
+    
+    case attribute_to_be_displayed
+    	when "1" then
+                  all_name = @db.display_all_name
+                  all_name
+                  main_menu
+    	when "2" then
+                  all_email = @db.display_all_email
+                  all_email
+                  main_menu
+    	when "3" then
+                  all_note = @db.display_all_note
+                  all_note
+                  main_menu               
+    	when "4" then
+                  all_id = @db.display_all_id
+                  all_id
+                  main_menu              
+    	else main_menu
+
+    	end
+  end
 
 	def exit_program
 		abort("Exiting BML CRM application")
@@ -90,5 +152,5 @@ class CRM
 end
 
 a_crm_app = CRM.new("crm")
+a_crm_app.clear_screen
 a_crm_app.main_menu
-
--- database.rb
+++ database.rb
@@ -27,5 +27,38 @@ class Database
     @contacts.select { |contact| @contacts.include?(id_to_find)}
   end
 
+  def modify_first_name(first_name, contact_to_be_modified)
+    @contacts.map { |contact| contact.first_name = first_name if contact.id == contact_to_be_modified}
+  end
 
+  def modify_last_name(last_name, contact_to_be_modified)
+    @contacts.map { |contact| contact.last_name = last_name if contact.id == contact_to_be_modified}
+  end
+
+  def modify_email(email, contact_to_be_modified)
+    @contacts.map { |contact| contact.email = email if contact.id == contact_to_be_modified}  
+  end
+
+  def modify_note(note, contact_to_be_modified)
+    @contacts.map { |contact| contact.note = note if contact.id == contact_to_be_modified} 
+  end
+
+  def display_all_name
+    @contacts.map { |contact| puts "[Name] :  #{contact.first_name} #{contact.last_name}" } 
+  end
+
+  def display_all_email
+    @contacts.map { |contact| puts "[Email] :  #{contact.email}"  } 
+  end
+
+  def display_all_note
+    @contacts.map { |contact| puts "[Note] :  #{contact.note}" } 
+  end
+
+  def display_all_id
+    @contacts.map { |contact| puts "[ID] :  #{contact.id}" } 
+  end
+
+  def modify_id(new_id, contact_to_be_modified)
+  end
 end
